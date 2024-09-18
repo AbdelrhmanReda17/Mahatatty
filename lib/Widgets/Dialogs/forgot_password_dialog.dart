a6bd74cfb9e199a01e7490f61774ce314230869c
Rana Helal <res.maika@gmail.com>
2024-09-19 00:38:47 +0300

    update forget,new pass

--- lib/Widgets/Dialogs/forgot_password_dialog.dart
+++ lib/Widgets/Dialogs/forgot_password_dialog.dart
@@ -1,15 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mahattaty/Utils/open_dialog.dart';
 import 'package:mahattaty/Widgets/Dialogs/otp_dialog.dart';
+import '../Generics/mahattaty_alert.dart';
 import '../Generics/mahattaty_dialog.dart';
 import '../Generics/mahattaty_text_form_field.dart';
+import 'new_password_dialog.dart';
 
-class ForgetPasswordDialog extends StatelessWidget {
-  ForgetPasswordDialog({super.key});
+class ForgetPasswordDialog extends StatefulWidget {
+  const ForgetPasswordDialog({super.key});
 
+  @override
+  State<ForgetPasswordDialog> createState() => _ForgetPasswordDialogState();
+}
+
+class _ForgetPasswordDialogState extends State<ForgetPasswordDialog> {
   final TextEditingController emailController = TextEditingController();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  bool emailSent = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,13 +34,79 @@ class ForgetPasswordDialog extends StatelessWidget {
           hintText: 'Enter your email',
           controller: emailController,
         ),
+        const SizedBox(height: 10),
+        if (emailSent)
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (emailSent)
+                RichText(
+                  text: TextSpan(
+                    text: 'Didn\'t receive an email? ',
+                    style: const TextStyle(color: Colors.red),
+                    children: [
+                      TextSpan(
+                        text: 'Resend Email',
+                        style: const TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () => _handlePasswordReset(context, isResend: true),
+                      ),
+                    ],
+                  ),
+                ),
+
+            ],
+          ),
+        const SizedBox(height: 10),
       ],
       buttonText: 'Send Email',
       contentPlacement: ContentPlacement.afterTitle,
-      onButtonPressed: () {
-        Navigator.of(context).pop();
-        openDialog(context: context, dialog: OTPDialog());
-      },
+      onButtonPressed: () { _handlePasswordReset(context);
+      Navigator.of(context).pop();
+      openDialog(context: context, dialog:  NewPasswordDialog());
+      }
     );
+  }
+
+  Future<void> _handlePasswordReset(BuildContext context, {bool isResend = false}) async {
+    final email = emailController.text.trim();
+
+    if (email.isEmpty) {
+      mahattatyAlertDialog(
+        context,
+        message: 'Email cannot be empty',
+        type: MahattatyAlertType.error,
+        onOk: () => Navigator.of(context).pop(),
+      );
+      return;
+    }
+
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+
+      mahattatyAlertDialog(
+        context,
+        message: isResend
+            ? 'Resent password reset link to $email'
+            : 'Password reset link sent to $email',
+        type: MahattatyAlertType.success,
+      );
+
+      if (!emailSent) {
+        setState(() {
+          emailSent = true;
+        });
+      }
+    } on FirebaseAuthException catch (e) {
+      mahattatyAlertDialog(
+        context,
+        message: e.message ?? 'Failed to send reset email',
+        type: MahattatyAlertType.error,
+      );
+    }
   }
 }
--- lib/Widgets/Dialogs/new_password_dialog.dart
+++ lib/Widgets/Dialogs/new_password_dialog.dart
@@ -1,16 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mahattaty/Utils/open_dialog.dart';
+import 'package:mahattaty/Utils/validate.dart';
 import 'package:mahattaty/Widgets/Dialogs/notification_dialog.dart';
 import 'package:mahattaty/Widgets/Generics/mahattaty_text_form_field.dart';
+import '../Generics/mahattaty_alert.dart';
 import '../Generics/mahattaty_dialog.dart';
 
 class NewPasswordDialog extends StatelessWidget {
   NewPasswordDialog({super.key});
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmPasswordController =
       TextEditingController();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -18,36 +23,63 @@ class NewPasswordDialog extends StatelessWidget {
       title: 'Create new password',
       description: 'Enter your new password and confirm it',
       content: [
-        MahattatyTextFormField(
-          labelText: 'Password',
-          isPassword: true,
-          iconData: FontAwesomeIcons.lock,
-          hintText: 'Create your password',
-          controller: passwordController,
-        ),
-        const SizedBox(height: 20),
-        MahattatyTextFormField(
-          labelText: 'Confirm Password',
-          isPassword: true,
-          iconData: FontAwesomeIcons.lock,
-          controller: confirmPasswordController,
-          hintText: 'Enter your password again',
+        Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              MahattatyTextFormField(
+                labelText: 'Password',
+                isPassword: true,
+                iconData: FontAwesomeIcons.lock,
+                hintText: 'Create your password',
+                controller: passwordController,
+                validator: (value) => value!.isValidPassword
+                    ? null
+                    : 'Password must be at least 8 characters, include an uppercase letter, a lowercase letter, and a number',
+              ),
+              const SizedBox(height: 20),
+              MahattatyTextFormField(
+                labelText: 'Confirm Password',
+                isPassword: true,
+                iconData: FontAwesomeIcons.lock,
+                controller: confirmPasswordController,
+                hintText: 'Enter your password again',
+                validator: (value) => value!.isValidConfirmPassword(passwordController.text)
+                    ? null
+                    : 'Passwords do not match',
+              ),
+            ],
+          ),
         ),
       ],
       buttonText: 'Change Password',
-      onButtonPressed: () {
-        Navigator.of(context).pop();
-        openDialog(
-          context: context,
-          dialog: const NotificationDialog(
-            title: 'Password Changed',
-            description:
-                'Your password has been changed successfully, you can now login with your new password',
-            icon: FontAwesomeIcons.solidCircleCheck,
-            buttonText: 'Continue to Login',
-            type: NotificationType.success,
-          ),
-        );
+      onButtonPressed: () async{
+        if (!_formKey.currentState!.validate()) {
+          return;
+        }
+        final newPassword = passwordController.text.trim();
+        final confirmPassword = confirmPasswordController.text.trim();
+
+        try {
+          final user = FirebaseAuth.instance.currentUser;
+          if (user != null) {
+            await user.updatePassword(newPassword);
+            mahattatyAlertDialog(
+              context,
+              message: 'Your password has been changed successfully.',
+              type: MahattatyAlertType.success,
+              onOk: () {
+                Navigator.of(context).pop();
+              },
+            );
+          }
+        } catch (e) {
+          mahattatyAlertDialog(
+            context,
+            message: 'Failed to change password. Please try again.',
+            type: MahattatyAlertType.error,
+          );
+        }
       },
     );
   }
