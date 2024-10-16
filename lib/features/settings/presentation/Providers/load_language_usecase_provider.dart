a11c40cce5595b9e0a77caef20692c27bd676272
Abdelrhman Reda <11410120210225@stud.cu.edu.eg>
2024-10-17 01:50:33 +0300

    delete load lang

--- lib/features/settings/presentation/Providers/load_language_usecase_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mahattaty/features/settings/presentation/Providers/settings_provider.dart';
-
-import '../../domain/usecases/load_lanuage_usecase.dart';
-final loadLanguageUseCaseProvider = Provider<LoadLanguageUseCase>((ref) {
-  final settingsRepository = ref.read(settingsRepositoryProvider);
-  return LoadLanguageUseCase(settingsRepository);
-});
